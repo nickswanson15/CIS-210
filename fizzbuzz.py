4ad1927469732eff7bda2c3aca842da7cea09ceb
nickswanson4 <88475946+nickswanson4@users.noreply.github.com>
2022-02-03 00:15:50 -0800

    Update and rename fizzbuzz to fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,12 +9,14 @@ Credits: N/A
 
 counts up to an integer n, and prints fizz if the number is divisble by three, buzz if divisible by 5, and fizzbuzz if divisible by both.
 '''
+
 def fb(n):
     '''docstring
         -   Integer > None  #type contract
 
         -  counts up to an integer n, and prints fizz if the number is divisble by three, buzz if divisible by 5, and fizzbuzz if divisible by both.Dispplays game  over if integer, n, is reached and the number if not divisible by 3 or 5.
     '''
+    
     for x in range(n):
         if x % 3 == 0 and x % 5 == 0:
             print("fizzbuzz")
